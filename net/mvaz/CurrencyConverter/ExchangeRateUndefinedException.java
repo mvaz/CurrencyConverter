e3ea5c4f8e283fcb31a389ee72845747b00e7725
Miguel Vaz <mvaz@dei.uminho.pt>
2009-11-09 02:10:27 +0100

    documentation

--- net/mvaz/CurrencyConverter/ExchangeRateUndefinedException.java
+++ net/mvaz/CurrencyConverter/ExchangeRateUndefinedException.java
@@ -1,29 +1,27 @@
 package net.mvaz.CurrencyConverter;
 
+/**
+ * custom Exception to signalize whenever that an exchange rate is not represented.
+ *  
+ * @author Miguel Vaz
+ */
 public class ExchangeRateUndefinedException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public ExchangeRateUndefinedException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ExchangeRateUndefinedException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ExchangeRateUndefinedException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ExchangeRateUndefinedException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
